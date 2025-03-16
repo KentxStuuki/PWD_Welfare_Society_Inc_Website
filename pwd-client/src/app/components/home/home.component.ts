d7f78181aadb78d9cf96fa08eff47d18bc77955f
qjsarah <165972652+qjsarah@users.noreply.github.com>
2025-03-16 20:39:56 +0800

    home page update

--- pwd-client/src/app/components/home/home.component.ts
+++ pwd-client/src/app/components/home/home.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { HomeEventsComponent } from './home-events/home-events.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HomeEventsComponent],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
